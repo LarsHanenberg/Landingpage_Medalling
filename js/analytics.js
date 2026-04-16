7e14aca7989cc6c03b8fdf4b05028c0c51365344
Lars Hanenberg <larshanenberg03@gmail.com>
2026-04-16 11:18:41 +0200

    Google-analytics

---
+++ js/analytics.js
@@ -0,0 +1,30 @@
+const GA_MEASUREMENT_ID = "G-XXXXXXXXXX";
+
+function setupGoogleAnalytics(measurementId) {
+    const trimmedId = measurementId.trim();
+    const hasPlaceholderId = trimmedId === "G-XXXXXXXXXX";
+
+    if (!trimmedId || hasPlaceholderId) {
+        console.info(
+            "Google Analytics staat klaar, maar is nog niet actief. Vervang GA_MEASUREMENT_ID in js/analytics.js door je eigen G-meet-ID."
+        );
+        return;
+    }
+
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        window.dataLayer.push(arguments);
+    }
+
+    window.gtag = gtag;
+    gtag("js", new Date());
+    gtag("config", trimmedId);
+
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(trimmedId)}`;
+    document.head.appendChild(script);
+}
+
+setupGoogleAnalytics(GA_MEASUREMENT_ID);
